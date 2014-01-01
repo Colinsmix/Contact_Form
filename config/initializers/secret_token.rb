28f80d82c78ed08040f51c7ea2ea1bb9117ee837
Colin Shields <N02433728@hawkmail.newpaltz.edu>
2014-01-01 15:48:00 -0500

    Created Database, Scaffold for Contacts

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ContactForm::Application.config.secret_key_base = 'ee1980394fb29c831f32b15e0489be904bd5d76499b956383a5d61e3f6f88b0c1471fff87308ebce07b62d2a806303c76ffcdfe4331ae37e5a962d89115b6932'
